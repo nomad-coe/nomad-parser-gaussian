3160b36d8eb7a550739b081295ccaa950c2d8ebd
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-03-30 15:16:21 +0200

    scalariform

--- src/main/scala/eu/nomad_lab/parsers/GaussianParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/GaussianParser.scala
@@ -1,37 +1,39 @@
 package eu.nomad_lab.parsers
 
-import eu.{nomad_lab=>lab}
+import eu.{ nomad_lab => lab }
 import eu.nomad_lab.DefaultPythonInterpreter
-import org.{json4s => jn}
+import org.{ json4s => jn }
 import scala.collection.breakOut
 
 object GaussianParser extends SimpleExternalParserGenerator(
-      name = "GaussianParser",
-      parserInfo = jn.JObject(
-        ("name" -> jn.JString("GaussianParser")) ::
-          ("parserId" -> jn.JString("GaussianParser" + lab.GaussianVersionInfo.version)) ::
-          ("versionInfo" -> jn.JObject(
-            ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
-              (lab.GaussianVersionInfo.toMap.map{ case (key, value) =>
-                (key -> jn.JString(value.toString))
-              }(breakOut): List[(String, jn.JString)])
-          )) :: Nil
-      ),
-      mainFileTypes = Seq("text/.*"),
-      mainFileRe = """\s*Gaussian, Inc\.  All Rights Reserved\.\s*
+  name = "GaussianParser",
+  parserInfo = jn.JObject(
+    ("name" -> jn.JString("GaussianParser")) ::
+      ("parserId" -> jn.JString("GaussianParser" + lab.GaussianVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+          (lab.GaussianVersionInfo.toMap.map {
+            case (key, value) =>
+              (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
+  mainFileTypes = Seq("text/.*"),
+  mainFileRe = """\s*Gaussian, Inc\.  All Rights Reserved\.\s*
 \s*
 \s*This is part of the Gaussian\(R\) [0-9]* program.""".r,
-      cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/gaussian/parser/parser-gaussian/parser_gaussian.py",
-        "--uri", "${mainFileUri}", "${mainFilePath}"),
-      resList = Seq(
-        "parser-gaussian/parser_gaussian.py",
-        "parser-gaussian/setup_paths.py",
-        "nomad_meta_info/common.nomadmetainfo.json",
-        "nomad_meta_info/meta_types.nomadmetainfo.json",
-        "nomad_meta_info/gaussian.nomadmetainfo.json"
-      ) ++ DefaultPythonInterpreter.commonFiles(),
-      dirMap = Map(
-        "parser-gaussian" -> "parsers/gaussian/parser/parser-gaussian",
-        "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
-        "python" -> "python-common/common/python/nomadcore") ++ DefaultPythonInterpreter.commonDirMapping()
+  cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/gaussian/parser/parser-gaussian/parser_gaussian.py",
+    "--uri", "${mainFileUri}", "${mainFilePath}"),
+  resList = Seq(
+    "parser-gaussian/parser_gaussian.py",
+    "parser-gaussian/setup_paths.py",
+    "nomad_meta_info/common.nomadmetainfo.json",
+    "nomad_meta_info/meta_types.nomadmetainfo.json",
+    "nomad_meta_info/gaussian.nomadmetainfo.json"
+  ) ++ DefaultPythonInterpreter.commonFiles(),
+  dirMap = Map(
+    "parser-gaussian" -> "parsers/gaussian/parser/parser-gaussian",
+    "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
+    "python" -> "python-common/common/python/nomadcore"
+  ) ++ DefaultPythonInterpreter.commonDirMapping()
 )
--- src/test/scala/eu/nomad_lab/parsers/GaussianParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/GaussianParserSpec.scala
@@ -1,19 +1,18 @@
 package eu.nomad_lab.parsers
 
-import eu.nomad_lab.{parsers, DefaultPythonInterpreter}
+import eu.nomad_lab.{ parsers, DefaultPythonInterpreter }
 import org.scalacheck.Properties
 import org.specs2.mutable.Specification
-import org.{json4s => jn}
-
+import org.{ json4s => jn }
 
 object GaussianParserSpec extends Specification {
   "GaussianParserTest" >> {
-    "test with Al.out">> {
+    "test with Al.out" >> {
       "test with json-events" >> {
-        ParserRun.parse(GaussianParser,"parsers/gaussian/test/examples/Al.out","json-events") must_== ParseResult.ParseSuccess
+        ParserRun.parse(GaussianParser, "parsers/gaussian/test/examples/Al.out", "json-events") must_== ParseResult.ParseSuccess
       }
       "test with json" >> {
-        ParserRun.parse(GaussianParser,"parsers/gaussian/test/examples/Al.out","json") must_== ParseResult.ParseSuccess
+        ParserRun.parse(GaussianParser, "parsers/gaussian/test/examples/Al.out", "json") must_== ParseResult.ParseSuccess
       }
     }
   }
