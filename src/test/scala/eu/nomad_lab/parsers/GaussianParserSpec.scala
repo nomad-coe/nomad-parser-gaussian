646717f2f4d94fb60a3b08293533967022c47e70
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-07-20 12:12:48 +0200

    Updated copyright info.

--- src/test/scala/eu/nomad_lab/parsers/GaussianParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/GaussianParserSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 Rosendo Valero, Fawzi Mohamed, Ankit Kariryaa, Rosendo Valero Montero
+ * Copyright 2015-2018 Rosendo Valero, Fawzi Mohamed, Ankit Kariryaa
  * 
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
